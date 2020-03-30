87fdb88dda098c8d7eaf75c995c5a6ec4eb97916
Mark <mark91002@gmail.com>
2020-03-30 14:50:58 +0800

    建立新的package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Example Package
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://github.com/hahatree/packaging_tutorial.git)
+to write your content.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Django", # Replace with your own username
+    version="1.0.0",
+    author="Mark",
+    author_email="NT87189@cathaybk.com.tw",
+    description="Django網頁框架",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hahatree/python.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
