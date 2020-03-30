1bcbbfdd1c8f68487d394d2314de053e11071e70
hahatree <45626735+hahatree@users.noreply.github.com>
2020-03-30 14:55:21 +0800

    Update setup.py
    修改url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Django網頁框架",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/hahatree/python.git",
+    url="https://github.com/hahatree/packaging_tutorial.git",
     packages=setuptools.find_packages(),
     classifiers=[
          "Programming Language :: Python",
